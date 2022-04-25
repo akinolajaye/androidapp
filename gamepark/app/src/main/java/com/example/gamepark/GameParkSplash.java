ccf4855663ee839a92458630653ac47d5898242f
akinolajaye <akinolajaye@gmail.com>
2022-04-25 18:15:47 +0100

    first

---
+++ gamepark/app/src/main/java/com/example/gamepark/GameParkSplash.java
@@ -0,0 +1,15 @@
+package com.example.gamepark;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameParkSplash extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this,MainActivity.class));
+    }
+}
