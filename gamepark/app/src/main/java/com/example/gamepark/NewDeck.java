b91acb27d05de1ca7fe3378dfa5e10f1d200d8f0
akinolajaye <akinolajaye@gmail.com>
2022-04-25 20:58:39 +0100

    added nav new deck

--- gamepark/app/src/main/java/com/example/gamepark/NewDeck.java
+++ gamepark/app/src/main/java/com/example/gamepark/NewDeck.java
@@ -15,10 +15,10 @@ import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LandingScreen#newInstance} factory method to
+ * Use the {@link NewDeck#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LandingScreen extends Fragment {
+public class NewDeck extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,7 +29,7 @@ public class LandingScreen extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LandingScreen() {
+    public NewDeck() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class LandingScreen extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LandingScreen.
+     * @return A new instance of fragment NewDeck.
      */
     // TODO: Rename and change types and number of parameters
-    public static LandingScreen newInstance(String param1, String param2) {
-        LandingScreen fragment = new LandingScreen();
+    public static NewDeck newInstance(String param1, String param2) {
+        NewDeck fragment = new NewDeck();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,22 +64,8 @@ public class LandingScreen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_landing_screen, container, false);
+        return inflater.inflate(R.layout.fragment_new_deck, container, false);
     }
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
 
-        NavController navController= Navigation.findNavController(view);
 
-        Button start=view.findViewById(R.id.new_deck_btn);
-        start.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                navController.navigate((R.id.action_landingScreen_to_newDeck));
-
-            }
-        });
-    }
 }
